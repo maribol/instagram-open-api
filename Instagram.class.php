1cfd658f6ad40ce1cf3c3b41f1965dbd6fbc0fce
Samuel Todosiciuc <maribol.ro@gmail.com>
2014-07-03 12:06:06 +0300

    Update Instagram.class.php

--- Instagram.class.php
+++ Instagram.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2013 Samuel Todosiciuc.
+ * Copyright (c) 2014 Samuel Todosiciuc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
